a2c31d1be024de8b89514d3afd17b6157b4a4a91
jwadlow2004 <jgvwadlow@gmail.com>
2024-11-25 16:47:35 +0000

    Spotted Done

--- GameDev3-Week4/Assets/_Scripts/Enemy/KinghtsView.cs
+++ GameDev3-Week4/Assets/_Scripts/Enemy/KinghtsView.cs
@@ -9,14 +9,16 @@ namespace GameDevWithMarco.Enemies
     {
 
         [SerializeField] GameEvent playerSpottedEvent;
-        
+        bool hasPlayerBeenSpotted = false;
         private void OnTriggerEnter(Collider other)
         {
-            if (other.tag == "Player")
+            if (other.tag == "Player" && !hasPlayerBeenSpotted)
             {
                 Debug.Log("Player Spotted");
 
                 playerSpottedEvent.Raise();
+
+                hasPlayerBeenSpotted = true;
             }
         }
 
